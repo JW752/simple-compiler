5f459610d172969cb40f79c528780fdddc89f62f
JW752 <48839262+JW752@users.noreply.github.com>
2019-03-22 14:35:43 -0700

    Add files via upload

---
+++ inputHandler/LocatedChar.java
@@ -0,0 +1,81 @@
+package inputHandler;
+
+/** Value object for holding a character and its location in the input text.
+ *  Contains delegates to select character operations.
+ *
+ */
+public class LocatedChar {
+	Character character;
+	TextLocation location;
+	
+	public LocatedChar(Character character, TextLocation location) {
+		super();
+		this.character = character;
+		this.location = location;
+	}
+
+	
+	//////////////////////////////////////////////////////////////////////////////
+	// getters
+	
+	public Character getCharacter() {
+		return character;
+	}
+	public TextLocation getLocation() {
+		return location;
+	}
+	public boolean isChar(char c) {
+		return character == c;
+	}
+	
+	
+	
+	//////////////////////////////////////////////////////////////////////////////
+	// toString
+	
+	public String toString() {
+		return "(" + charString() + ", " + location + ")";
+	}
+	private String charString() {
+		if(Character.isWhitespace(character)) {
+			int i = character;
+			return String.format("'\\%d'", i);
+		}
+		else {
+			return character.toString();
+		}
+	}
+
+	
+	//////////////////////////////////////////////////////////////////////////////
+	// delegates
+	
+	public boolean isLowerCase() {
+		return Character.isLowerCase(character) || (character == '_');
+	}
+	public boolean isUpperCase() {
+		return Character.isUpperCase(character) || (character == '_');
+	}
+//	public boolean isIdentierStart() {
+//		return Character.isidentifiers;
+//	}
+//	public boolean isIdentifierContinue() {
+//		
+//	}
+//	public boolean isNumberStart() {
+//		return Character.is
+//	}
+	public boolean isDigit() {
+		return Character.isDigit(character);
+	}
+	public boolean isSign() {
+		return (character == '+' || character == '-');
+	}
+	public boolean isWhitespace() {
+		return Character.isWhitespace(character);
+	}
+	public boolean isASCII() {
+		int n = character;
+		return ((n >= 32) && (n <= 126));
+	}
+}
